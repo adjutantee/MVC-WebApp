30e0c3173ad4ab06226428ed5ee0411251b0cff8
Izagakhmaev Ramazan <78782750+adjutantee@users.noreply.github.com>
2022-08-24 16:51:37 +0300

    sin

--- MVC-WebApp/Startup.cs
+++ MVC-WebApp/Startup.cs
@@ -25,7 +25,6 @@ namespace MVC_WebApp
             services.AddControllersWithViews();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
