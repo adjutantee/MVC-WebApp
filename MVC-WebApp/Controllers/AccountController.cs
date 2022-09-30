54d690fd455537d60e3533da35b4a7672039ce17
Ramazan Izagakhmaev <izagakhmaevra@gmail.com>
2022-09-30 14:17:32 +0300

    Add login views form

---
+++ MVC-WebApp/Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_WebApp.Models;
+
+namespace MVC_WebApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(Login login)
+        {
+            return RedirectToAction("Index", "Home");
+        }
+        
+    }
+}
