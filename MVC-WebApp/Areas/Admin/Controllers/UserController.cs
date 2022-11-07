5c822f9e3845400bcf589aef6da2775dd9bad9fa
Ramazan Izagakhmaev <izagakhmaevra@gmail.com>
2022-11-07 15:19:16 +0300

    save

--- MVC-WebApp/Areas/Admin/Controllers/UserController.cs
+++ MVC-WebApp/Areas/Admin/Controllers/UserController.cs
@@ -1,13 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MVC_WebApp.Services;
 
 namespace MVC_WebApp.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class UserController : Controller
     {
+        private readonly IUserManager userManager;
+
+        public UserController(IUserManager userManager)
+        {
+            this.userManager = userManager;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var userAccounts = userManager.GetAllUsers();
+            return View(userAccounts);
         }
     }
 }
