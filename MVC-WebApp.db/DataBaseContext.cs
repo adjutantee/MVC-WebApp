5b80a8608fc7b61727fab361de1fac48464ba029
Ramazan Izagakhmaev <izagakhmaevra@gmail.com>
2022-11-08 16:54:22 +0300

    save base

---
+++ MVC-WebApp.db/DataBaseContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC_WebApp.db.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_WebApp.db
+{
+    public class DataBaseContext : DbContext
+    {
+        // Доступ к таблицам. Каждый DbSet это доступ к какой то одной таблице.
+        public DbSet<Product> Products { get; set; }
+
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
+        {
+            Database.EnsureCreated(); // Создаем базу при первом обращении
+        }
+    }
+}
