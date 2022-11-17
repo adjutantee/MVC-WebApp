1dd0a90e5e41197287949a5dd2c9ede9f92e87fe
Ramazan Izagakhmaev <izagakhmaevra@gmail.com>
2022-11-17 14:01:50 +0300

    save-POINT

---
+++ MVC-WebApp.db/IdentityContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MVC_WebApp.db.Models;
+
+namespace MVC_WebApp.db
+{
+    public class IdentityContext : IdentityDbContext<User>
+    {
+        public IdentityContext(DbContextOptions<IdentityContext> options) : base(options)
+        {
+            Database.Migrate();
+        }
+    }
+}
---
+++ MVC-WebApp.db/Models/User.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MVC_WebApp.db.Models
+{
+    public class User : IdentityUser
+    {
+    }
+}
